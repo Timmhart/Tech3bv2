237b732eb579729905df5feb0169f82804075fea
Timmhart <tim.hart@hva.nl>
2021-03-01 13:51:52 +0100

    changes made with some ejs files

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const ejs = require('ejs');
 const app = express();
 const port = 3000;
 
-app.use('/static', express.static('public'));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.send('hello world');
 });
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
 
 app.use(function (req, res, next) {
     res.status(404).send("Sorry can't find that!")
