9b0b80c0e7df512d4f0329774ce09c35262e0df3
Timmhart <tim.hart@hva.nl>
2021-03-08 13:16:34 +0100

    static files changed

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/images', express.static(__dirname + 'public/images'));
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.render('login');
 });
 
-app.get('/login', (req, res) => {
-    res.render('login')
-})
 
 app.use(function (req, res, next) {
     res.status(404).send("Sorry can't find that!")
