fb3a87956c380947ec1e262031318cb3545d534e
Timmhart <tim.hart@hva.nl>
2021-04-01 19:49:06 +0200

    connected to database

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const slug = require('slug');
 const app = express();
 const dotenv = require('dotenv').config();
+const { MongoClient } = require('mongodb');
 const port = 3000;
 
-console.log(process.env.TESTVAR);
+let db = null;
+// function connectDB
+async function connectDB () {
+    // get URI from .env file
+    const uri = process.env.DB_URI
+    // make connection to database
+    const options = { useUnifiedTopology: true};
+    const client = new MongoClient(uri, options);
+    await client.connect();
+    db = await client.db(process.env.DB_NAME);
+};
 
+connectDB()
+.then(() => {
+    // if succesfull connection is made, show a message
+    console.log('We have a connection to Mongo!');
+})
+.catch( error => {
+    console.log(error);
+});
+
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.static('public'));
-
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
