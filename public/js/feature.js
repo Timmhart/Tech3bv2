d7b7ef63dc5181be750efe01465ad26449fafaa7
Timmhart <tim.hart@hva.nl>
2021-04-07 23:36:49 +0200

    fixed db

--- public/js/feature.js
+++ public/js/feature.js
@@ -1,14 +1,3 @@
-// const profilePicture = document.querySelector('playerImg');
-// const profilePictures = ['../images/alex.jpeg', '../images/alice.jpeg', '../images/charly.jpeg', '../images/d-day.jpeg', '../images/domino.jpeg',
-// '../images/farah.jpeg', '../images/ghost.jpeg', '../images/mara.jpeg', '../images/morte.jpeg', '../images/price.jpeg'];
-
-// function randomProfile(profilePictures) {
-//     profilePicture = Math.floor(Math.random() * profilePictures.length);
-// };
-
-// console.log(profilePicture)
-    
-
 const hamburgerMenu = document.querySelector('#menu');
 
 function onClickMenu() {
